0ef0ac33f06d594390d106258f3f40dee8cc84df
FurkanKaya10 <fk123455445@gmail.com>
2025-12-07 16:14:19 +0300

    Add formatted output to ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+    printf("%-10s %-10s %-10s\n", "Column 1", "Column 2", "Column 3" );
+    printf("\n");
+  
+    for (int i = 1; i <= 10; i++){
+  
+        int col1 = i * 5;
+        int col2 = 100 - ((i - 1) * 10 + 1);
+        int col3 = i;
 
+        printf("%-10d %-10d %-10d\n", col1, col2, col3);
+    }
+
+    return 0;
+}
