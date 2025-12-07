87aa1f688063c60d77acffcb9914942b719e954c
FurkanKaya10 <fk123455445@gmail.com>
2025-12-07 16:12:09 +0300

    update 6.c
    Add program to print numbers divisible by 3 from 99 to 51.

--- ex06.c
+++ ex06.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+          int main() {
+ int numbers[17];
+ int index =0;
 
+ for(int i = 99; i>=51; i-=3){
+    numbers[index] = i;
+    index++;
+   }
+
+ printf("Numbers divisible by 3 from 100 down to 50: \n");
+ 
+ for (int j =0; j<index; j++) {
+     printf("%d", numbers[j]);
+   if (j< index - 1) {
+     printf("\n");
+    }
+  }
+  printf("\n");          
+}
